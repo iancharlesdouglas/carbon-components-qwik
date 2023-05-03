0379e0284a3e28fe8edca1f51d792b60437aa379
Ian Douglas <ian@iandouglas.info>
2023-05-03 11:14:58 +0100

    Added vitest and starter unit test for Link component

---
+++ src/components/link/link.test.tsx
@@ -0,0 +1,39 @@
+import {describe, expect, it} from 'vitest';
+import { Link } from './link';
+import { createDOM } from '@builder.io/qwik/testing';
+import { CarbonApp } from '../carbon-app/carbon-app';
+
+describe('Link', () => {
+  it('renders href, text content in its anchor and link CSS class', async () => {
+    const {screen, render} = await createDOM();
+    const url = 'github.com';
+    const content = 'GitHub';
+
+    await render(<CarbonApp><Link href={url}>{content}</Link></CarbonApp>);
+    
+    const aElement = screen.querySelector('a') as HTMLAnchorElement;
+    expect(aElement.getAttribute('href')).toEqual(url);
+    expect(aElement.textContent).toEqual(content);
+    expect(aElement.getAttribute('class')).toEqual('cds--link');
+  });
+
+  it('supports a custom CSS class', async () => {
+    const {screen, render} = await createDOM();
+    const customClass = 'custom-format';
+
+    await render(<CarbonApp><Link href="x.com" class={customClass}>Custom text</Link></CarbonApp>);
+
+    const aElement = screen.querySelector('a') as HTMLAnchorElement;
+    expect(aElement.classList.contains(customClass));
+  });
+
+  it('sets disabled CSS class if disabled', async () => {
+    const {screen, render} = await createDOM();
+
+    await render(<CarbonApp><Link href="x.com" disabled>X.com</Link></CarbonApp>)
+
+    const aElement = screen.querySelector('a') as HTMLAnchorElement;
+
+    expect(aElement.classList.contains('cds--link--disabled'));
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { Logo } from './components/logo/logo';
 export { Counter } from './components/counter/counter';
+export { Link } from './components/link/link';
