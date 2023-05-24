179be62a9be98aaa2e3954234c66807a044e9e82
Ian Douglas <ian@iandouglas.info>
2023-05-24 09:12:13 +0100

    Added ListBox component and covering tests

---
+++ src/components/list-box/list-box.test.tsx
@@ -0,0 +1,115 @@
+import { describe, expect, it } from 'vitest';
+import { createDOM } from '@builder.io/qwik/testing';
+import { CarbonRoot } from '../carbon-root/carbon-root';
+import { ListBox } from './list-box';
+import { Form } from '../form/form';
+
+describe('ListBox', () => {
+  it('renders expected CSS class and custom class', async () => {
+    const { screen, render } = await createDOM();
+    const customClass = 'custom-class';
+    const listBoxId = 'list-box-id';
+
+    await render(
+      <CarbonRoot>
+        <Form>
+          <ListBox class={customClass} id={listBoxId}></ListBox>
+        </Form>
+      </CarbonRoot>
+    );
+
+    const divElement = screen.querySelector(`div#${listBoxId}`) as HTMLDivElement;
+    expect(divElement.classList.contains('cds--list-box')).toBeTruthy();
+    expect(divElement.classList.contains(customClass)).toBeTruthy();
+  });
+
+  it('renders disabled, expanded and inline states as expected', async () => {
+    const { screen, render } = await createDOM();
+    const listBoxId = 'list-box-id';
+
+    await render(
+      <CarbonRoot>
+        <Form>
+          <ListBox disabled isOpen type="inline" id={listBoxId}></ListBox>
+        </Form>
+      </CarbonRoot>
+    );
+
+    const divElement = screen.querySelector(`div#${listBoxId}`) as HTMLDivElement;
+    expect(divElement.classList.contains('cds--list-box--inline')).toBeTruthy();
+    expect(divElement.classList.contains('cds--list-box--disabled')).toBeTruthy();
+    expect(divElement.classList.contains('cds--list-box--expanded')).toBeTruthy();
+  });
+
+  it('renders invalid state as expected', async () => {
+    const { screen, render } = await createDOM();
+    const listBoxId = 'list-box-id';
+    const invalidMessage = 'Please select an option';
+
+    await render(
+      <CarbonRoot>
+        <Form>
+          <ListBox invalid invalidText={invalidMessage} id={listBoxId}></ListBox>
+        </Form>
+      </CarbonRoot>
+    );
+
+    const divElement = screen.querySelector(`div#${listBoxId}`) as HTMLDivElement;
+    expect(divElement.classList.contains('cds--list-box--invalid')).toBeTruthy();
+    const invalidElement = screen.querySelector(`div#${listBoxId} div.cds--form-requirement`) as HTMLDivElement;
+    expect(invalidElement.textContent).toEqual(invalidMessage);
+  });
+
+  it('renders warning state as expected', async () => {
+    const { screen, render } = await createDOM();
+    const listBoxId = 'list-box-id';
+    const warningMessage = 'Please select a better option';
+
+    await render(
+      <CarbonRoot>
+        <Form>
+          <ListBox warn warnText={warningMessage} id={listBoxId}></ListBox>
+        </Form>
+      </CarbonRoot>
+    );
+
+    const divElement = screen.querySelector(`div#${listBoxId}`) as HTMLDivElement;
+    expect(divElement.classList.contains('cds--list-box--warning')).toBeTruthy();
+    const warningElement = screen.querySelector(`div#${listBoxId} div.cds--form-requirement`) as HTMLDivElement;
+    expect(warningElement.textContent).toEqual(warningMessage);
+  });
+
+  it('removes non-standard attributes prior to rendering', async () => {
+    const { screen, render } = await createDOM();
+    const listBoxId = 'list-box-id';
+
+    await render(
+      <CarbonRoot>
+        <Form>
+          <ListBox id={listBoxId}></ListBox>
+        </Form>
+      </CarbonRoot>
+    );
+
+    const divElement = screen.querySelector(`div#${listBoxId}`) as HTMLDivElement;
+    const nonStdAttrs = ['disabled', 'invalid', 'invalidText', 'isOpen', 'size', 'type', 'warn', 'warnText'];
+    nonStdAttrs.forEach((attr) => expect(divElement.hasAttribute(attr)).toBeFalsy());
+  });
+
+  it('triggers key-down', async () => {
+    const { screen, render, userEvent } = await createDOM();
+    const listBoxId = 'list-box-id';
+
+    await render(
+      <CarbonRoot>
+        <Form>
+          <ListBox id={listBoxId}></ListBox>
+        </Form>
+      </CarbonRoot>
+    );
+
+    const divElement = screen.querySelector(`div#${listBoxId}`) as HTMLDivElement;
+    await userEvent(divElement, 'keydown', { keyCode: 27 });
+    await userEvent(divElement, 'keydown', { keyCode: 13 });
+  });
+});
---
+++ src/components/list-box/list-box.tsx
@@ -0,0 +1,67 @@
+import { QwikIntrinsicElements, QwikKeyboardEvent, component$, useContext, $ } from '@builder.io/qwik';
+import { usePrefix } from '../../internal/hooks/use-prefix';
+import { formContext } from '../../internal/contexts/form-context';
+import classNames from 'classnames';
+import _ from 'lodash';
+
+/**
+ * ListBox props
+ * @property {string} class - Custom CSS class
+ * @property {boolean} disabled - Whether list box is disabled
+ * @property {boolean} invalid - Whether list box is in an invalid state
+ * @property {string} invalidText - Invalid state message
+ * @property {boolean} isOpen - Whether list box is currently open
+ * @property {string} size - Size
+ * @property {string} type - Either 'default' (the default) or 'inline'
+ * @property {boolean} warn - Whether list box is in a warning state
+ * @property {string} warnText - Warning state message
+ */
+export type ListBoxProps = QwikIntrinsicElements['div'] & {
+  class?: string;
+  disabled?: boolean;
+  invalid?: boolean;
+  invalidText?: string;
+  isOpen?: boolean;
+  size?: 'sm' | 'md' | 'lg';
+  type?: 'default' | 'inline';
+  warn?: boolean;
+  warnText?: string;
+};
+
+/**
+ * ListBox component
+ */
+export const ListBox = component$((props: ListBoxProps) => {
+  const prefix = usePrefix();
+  const { isFluid } = useContext(formContext);
+  const { class: customClassName, size, type = 'default', disabled = false, isOpen = false, invalid = false, invalidText, warn, warnText } = props;
+  const showWarning = !invalid && warn;
+  const classes = classNames(
+    customClassName,
+    `${prefix}--list-box`,
+    { [`${prefix}--list-box--${size}`]: !!size },
+    { [`${prefix}--list-box--inline`]: type === 'inline' },
+    { [`${prefix}--list-box--disabled`]: disabled },
+    { [`${prefix}--list-box--expanded`]: isOpen },
+    { [`${prefix}--list-box--invalid`]: invalid },
+    { [`${prefix}--list-box--warning`]: showWarning }
+  );
+  const sanitizedProps = _.omit(props, 'class', 'disabled', 'invalid', 'invalidText', 'isOpen', 'size', 'type', 'warn', 'warnText');
+  return (
+    <div
+      {...sanitizedProps}
+      class={classes}
+      onKeyDown$={$((event: QwikKeyboardEvent<HTMLDivElement>) => {
+        if (event.keyCode === 27) {
+          event?.stopPropagation();
+        }
+      })}
+      preventdefault:click
+      data-invalid={invalid}
+    >
+      {isFluid && <hr class={`${prefix}--list-box__divider`} />}
+      {invalid && <div class={`${prefix}--form-requirement`}>{invalidText}</div>}
+      {showWarning && <div class={`${prefix}--form-requirement`}>{warnText}</div>}
+    </div>
+  );
+});
