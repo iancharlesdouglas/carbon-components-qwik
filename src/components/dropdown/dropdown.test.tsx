e0a794b7622405b5d1c473aea9f37330ba088a1e
Ian Douglas <ian@iandouglas.info>
2023-05-28 20:38:16 +0100

    Dropdown - WIP

---
+++ src/components/dropdown/dropdown.test.tsx
@@ -0,0 +1,99 @@
+import { describe, expect, it } from 'vitest';
+import { createDOM } from '@builder.io/qwik/testing';
+import { CarbonRoot } from '../carbon-root/carbon-root';
+import { Form } from '../form/form';
+import { Dropdown } from './dropdown';
+
+describe('Dropdown', () => {
+  it('renders expected CSS classes per attributes', async () => {
+    const { screen, render } = await createDOM();
+    const customClass = 'custom-class';
+    const dropdownId = 'dropdown-id';
+
+    await render(
+      <CarbonRoot>
+        <Form>
+          <Dropdown id={dropdownId} class={customClass} invalid type="inline" disabled direction="top" />
+        </Form>
+      </CarbonRoot>
+    );
+
+    const divElement = screen.querySelector(`div#${dropdownId}`) as HTMLDivElement;
+    expect(divElement.classList.contains('cds--dropdown')).toBeTruthy();
+    expect(divElement.classList.contains('cds--dropdown--invalid')).toBeTruthy();
+    expect(divElement.classList.contains('cds--dropdown--warning')).toBeFalsy();
+    expect(divElement.classList.contains('cds--dropdown--inline')).toBeTruthy();
+    expect(divElement.classList.contains('cds--dropdown--disabled')).toBeTruthy();
+    expect(divElement.classList.contains('cds--list-box--up')).toBeTruthy();
+  });
+
+  it('renders warning CSS class if in warning state', async () => {
+    const { screen, render } = await createDOM();
+    const dropdownId = 'dropdown-id';
+
+    await render(
+      <CarbonRoot>
+        <Form>
+          <Dropdown id={dropdownId} warn />
+        </Form>
+      </CarbonRoot>
+    );
+
+    const divElement = screen.querySelector(`div#${dropdownId}`) as HTMLDivElement;
+    expect(divElement.classList.contains('cds--dropdown--warning')).toBeTruthy();
+    expect(divElement.classList.contains('cds--dropdown--invalid')).toBeFalsy();
+  });
+
+  it('omits non-standard properties from the rendered div element', async () => {
+    const { screen, render } = await createDOM();
+    const dropdownId = 'dropdown-id';
+
+    await render(
+      <CarbonRoot>
+        <Form>
+          <Dropdown
+            id={dropdownId}
+            class="some-class"
+            direction="bottom"
+            disabled
+            helperText="Enter a name"
+            hideLabel={false}
+            invalid
+            invalidText="Invalid!"
+            label="Name"
+            renderSize="md"
+            titleText="Name input"
+            type="default"
+            warn
+            warnText="Ugly name"
+          />
+        </Form>
+      </CarbonRoot>
+    );
+
+    const divElement = screen.querySelector(`div#${dropdownId}`) as HTMLDivElement;
+    const illegalAttrs = [
+      'direction',
+      'disabled',
+      'helperText',
+      'hideLabel',
+      'initialSelectedItem',
+      'invalid',
+      'invalidText',
+      'itemToElement$',
+      'itemToString',
+      'items',
+      'label',
+      'onChange$',
+      'renderSelectedItem$',
+      // 'renderSize',
+      'selectedItem',
+      'titleText',
+      'translateWithId',
+      'type',
+      'warn',
+      'warnText',
+    ];
+    illegalAttrs.forEach((attr) => expect(divElement.hasAttribute(attr), `Attribute: ${attr} unexpected`).toBeFalsy());
+  });
+});
--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -1,16 +1,33 @@
 import { Component, PropFunction, QwikIntrinsicElements, component$ /*, useContext, useSignal*/ } from '@builder.io/qwik';
-// import { usePrefix } from '../../internal/hooks/use-prefix';
+import { usePrefix } from '../../internal/hooks/use-prefix';
 // import { formContext } from '../../internal/contexts/form-context';
-// import classNames from 'classnames';
+import classNames from 'classnames';
+import _ from 'lodash';
+
+/**
+ * List item type
+ */
+export type Item = string | { label: string };
+
+/**
+ * Function that takes an item and returns a string representation of it
+ */
+export type ItemToString = (item: Item) => string;
+
+// const defaultItemToString: ItemToString = (item: Item) => {
+//   if (typeof item === 'string') {
+//     return item;
+//   }
+//   return item ? item.label : '';
+// };
 
 /**
  * Dropdown props
  */
 export type DropdownProps = QwikIntrinsicElements['select'] & {
-  // class?: string; - LEFT OUT Of v11
+  class?: string;
   direction?: 'top' | 'bottom';
   disabled?: boolean;
-  // downshiftProps: ??
   helperText?: string;
   hideLabel?: boolean;
   id?: string;
@@ -18,13 +35,13 @@ export type DropdownProps = QwikIntrinsicElements['select'] & {
   invalid?: boolean;
   invalidText?: string;
   itemToElement$?: Component<any>;
-  itemToString?: any; // function to return 'label' attribute by default but to return string from item
-  items?: any[];
+  itemToString?: ItemToString;
+  items?: Item[];
   label?: string;
   onChange$?: PropFunction;
   renderSelectedItem$?: Component<any>;
+  renderSize?: 'sm' | 'md' | 'lg';
   selectedItem?: object | string | number;
-  size?: 'sm' | 'md' | 'lg';
   titleText?: string;
   translateWithId?: () => string;
   type?: 'default' | 'inline';
@@ -33,25 +50,60 @@ export type DropdownProps = QwikIntrinsicElements['select'] & {
 };
 
 /**
- * Dropdown
+ * Dropdown, a select-only combobox
  */
-export const Dropdown = component$((/*props: DropdownProps*/) => {
-  // const prefix = usePrefix();
+export const Dropdown = component$((props: DropdownProps) => {
+  const prefix = usePrefix();
   // const { isFluid } = useContext(formContext);
-
-  // // TODO - select props?
-
-  // const { type = 'default', invalid = false, warn = false } = props;
-  // const inline = type === 'inline';
-  // const showWarning = !invalid && warn;
+  const {
+    type = 'default',
+    invalid = false,
+    warn = false,
+    disabled = false,
+    renderSize: size = 'md',
+    direction = 'bottom' /*, itemToString = defaultItemToString */,
+  } = props;
+  const inline = type === 'inline';
+  const showWarning = !invalid && warn;
 
   // const isFocused = useSignal(false);
 
-  // const isOpen = false; // TODO - remove
-  // const classes = classNames(`${prefix}--dropdown`, {
-  //   [`${prefix}--dropdown--invalid`]: invalid,
-  //   [`${prefix}--dropdown--warning`]: showWarning,
-  //   [`${prefix}--dropdown--open`]: isOpen,
-  // });
-  return <></>;
+  const isOpen = false;
+
+  const classes = classNames(`${prefix}--dropdown`, {
+    [`${prefix}--dropdown--invalid`]: invalid,
+    [`${prefix}--dropdown--warning`]: showWarning,
+    [`${prefix}--dropdown--open`]: isOpen,
+    [`${prefix}--dropdown--inline`]: inline,
+    [`${prefix}--dropdown--disabled`]: disabled,
+    [`${prefix}--dropdown--${size}`]: size,
+    [`${prefix}--list-box--up`]: direction === 'top',
+  });
+
+  const sanitizedProps = _.omit(
+    props,
+    'class',
+    'direction',
+    'disabled',
+    'helperText',
+    'hideLabel',
+    'initialSelectedItem',
+    'invalid',
+    'invalidText',
+    'itemToElement$',
+    'itemToString',
+    'items',
+    'label',
+    'onChange$',
+    'renderSelectedItem$',
+    'renderSize',
+    'selectedItem',
+    'titleText',
+    'translateWithId',
+    'type',
+    'warn',
+    'warnText'
+  );
+
+  return <div class={classes} {...sanitizedProps}></div>;
 });
